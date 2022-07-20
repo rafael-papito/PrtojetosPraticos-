6e65a36510e5a0e821dddfdc61e2c452a4db299c
Rafael Porto <73806403+rafael-papito@users.noreply.github.com>
2022-07-20 11:01:02 -0300

    esse foi o meu primeiro projeto pratico usando css e html, um completo desastre, mais eu tenho bastante apreço por ele

---
+++ Projeto01/JS/projeto01.js
@@ -0,0 +1,13 @@
+$(function(){
+
+    $('.mobile').click(function(){
+        $('.mobile ul').slideToggle();
+    })
+
+    $('nav a').click(function(){
+        var href = $(this).attr('href');
+        var offSetTop = $(href).offSet().top;
+        $('html,body').animate('scrollTop' + offSetTop);
+        return false
+    })
+ })
