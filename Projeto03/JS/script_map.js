819baf03307ea650ba25b86d635a84a9510c375a
Rafael Porto <73806403+rafael-papito@users.noreply.github.com>
2022-07-22 09:24:23 -0300

    esse foi o 3° projetinho pratico desenvolvido por mim, vocês ja veem que vai melhorando com a prática

---
+++ Projeto03/JS/script_map.js
@@ -0,0 +1,13 @@
+window.onload = function(){
+   var map;
+   function initialize(){
+       var mapProp = {
+           center:new google.maps.LatLng(-27.648598,-48.577423),
+           scrollWheel: false,
+           zoom:12, 
+           MapTypeId:google.maps.MapTypeId.ROADMAP
+       }
+       map = new google.maps.Map(document.getElementById("mapa"),mapProp)
+   }
+   initialize();
+}
